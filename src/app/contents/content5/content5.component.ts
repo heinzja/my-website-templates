2614fdc195990e022334aa4466ca259dd0226f8d
heinzja <joseph.heinz17@outlook.com>
2021-09-28 09:17:08 -0500

    -- implemented content5

--- src/app/contents/content5/content5.component.ts
+++ src/app/contents/content5/content5.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './content5.component.html',
@@ -6,9 +6,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Content5Component implements OnInit {
 
-  constructor() { }
+  current_date: Date = new Date();
+
+  screenWidth: number = 0;
+  screenHeight: number = 0;
+
+  cardCssStyles = "card-md";
+
+  constructor() {
+    this.setScreenHeightWidthValues();
+   }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.setScreenHeightWidthValues();
+  }
+
+  getRandomViewCount(): number {
+    var val: number = 0;
+    val = Math.floor(Math.random() * 1000000)
+    return val;
+  }
+
+  setScreenHeightWidthValues() {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+    if(this.screenWidth <= 500){
+      this.cardCssStyles = "card-xs";
+    }
+    else if(this.screenWidth <= 760){
+      this.cardCssStyles = "card-sm";
+    }
+    else if (this.screenWidth <= 960) {
+      this.cardCssStyles = "card-md";
+    } else {
+      this.cardCssStyles = "card-lg";
+    }
   }
 
 }
