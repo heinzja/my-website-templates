2947b54c306ac543d5deefb907aa41aa8b843ecb
heinzja <joseph.heinz17@outlook.com>
2021-09-28 17:19:31 -0500

    -- added missing deps to material.module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -13,10 +13,14 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatTableModule} from '@angular/material/table';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [],
   imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
@@ -34,6 +38,8 @@ import { NgModule } from '@angular/core';
     MatTableModule,
   ],
   exports: [
+    BrowserModule,
+    BrowserAnimationsModule,
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
